1a1d105227b448958eb29b16f58c1c7e2dc88d51
BifolRomania <64065636+BifolRomania@users.noreply.github.com>
2025-08-15 13:29:30 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# --- TELEGRAM ---
+TELEGRAM_API_ID = 20676315         # De la https://my.telegram.org
+TELEGRAM_API_HASH = "0ecd754a3b356e1ed4bc09a9c4b82484"
+BOT_TOKEN = "8371712508:AAEdgWPxRmMZk8uzEBpYwcYPCQIPnPlTmwo"     # Botul care îți scrie în privat
+GROUP_ID = -4796546791        # ID-ul grupului de monitorizat
+
+# --- GEMINI ---
+GEMINI_API_KEY = "AIzaSyC8baKUo6aHzpOt7MAd0g1Zr99Y6pSTqHs"  # Dacă rămâi fără token, folosește regex fallback
+
+# --- USER SETTINGS ---
+YOUR_NAME = "Viorel"
+ADMIN_CHAT_ID = 1899963354        # ID-ul tău Telegram
